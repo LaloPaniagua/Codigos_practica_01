b05d5fdbc143770648477d67426f266f9bf6454d
Barraganri <80717543+Barraganri@users.noreply.github.com>
2021-03-15 17:04:19 -0700

    Update Prac1p2.c

--- Prac1p2.c
+++ Prac1p2.c
@@ -3,15 +3,15 @@
 int main()
 	{
 		int array2[3][3],i,j;
-		for(i=0;i<3;i++)
+		for(i=0;i<3;i++)      //Ciclo for para renglones
 			{
-				for(j=0;j<3;j++)
+				for(j=0;j<3;j++)    //Ciclo for para columnas
 					{
-						printf("Dirección en memoria del elemento (%i, %i): %p\n",i,j,&array2[i][j] );
+						printf("DirecciÃ³n en memoria del elemento (%i, %i): %p\n",i,j,&array2[i][j] );
 					}
 				printf("\n");
 			}
-		printf("Dirección en memoria del primer elemento: %p\n",&array2[0][0]);
-		printf("Dirección en memoria del arreglo: %p",&array2);
+		printf("DirecciÃ³n en memoria del primer elemento: %p\n",&array2[0][0]);
+		printf("DirecciÃ³n en memoria del arreglo: %p",&array2);
 		return 1;
 	}
