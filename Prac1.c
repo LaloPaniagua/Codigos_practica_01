9a6464b8fd61bd6d79f8d397eecc0389763ed149
Barraganri <80717543+Barraganri@users.noreply.github.com>
2021-03-15 17:01:45 -0700

    Prac1.c
    -Se agregaron notas

--- Prac1.c
+++ Prac1.c
@@ -1,9 +1,14 @@
 #include<stdio.h>
 #include<stdlib.h>
+/*
+*
+*
+*
+*/
 int main()
 {
 	int array1[10],i;
-	for(i=0;i<10;i++) //se crea un for para imprimir todas las direcciones de memoria de los elementos del arreglo 
+	for(i=0;i<10;i++) //se crea un ciclo for para imprimir todas las direcciones de memoria de los elementos del arreglo 
 		{
 			printf("Dirección de memoria del elemento %i: %p\n",i,&array1[i]);                              
 		}
